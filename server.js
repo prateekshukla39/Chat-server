5b32fbf596862004a9a313cd22d4930e823b3f4a
Prateek Shukla <54330564+prateekshukla39@users.noreply.github.com>
2023-06-28 16:20:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const net = require("net");
 const server = net.createServer();
 
 const PORT = 4033;
-const HOST = "216.24.57.253";
+const HOST = "127.0.0.1";
 
 // An Array of client/socket objects 
 const clients = [];
