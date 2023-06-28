efa385098087939307b1379f9650dc73d1b69b50
Prateek Shukla <54330564+prateekshukla39@users.noreply.github.com>
2023-06-28 15:22:45 +0530

    server.js added

---
+++ server.js
@@ -0,0 +1,43 @@
+const net = require("net");
+
+const server = net.createServer();
+
+const PORT = 4033;
+const HOST = "10.0.4.7";
+
+// An Array of client/socket objects 
+const clients = [];
+
+server.on("connection", (socket) => {
+    console.log("A new connection to the server!");
+    const clientId = clients.length + 1;
+
+    // broadcasting a message when a user joins
+    clients.map((client) => {
+        client.socket.write(`User ${clientId} joined!`)
+    });
+    socket.write(`Id-${clientId}`);
+
+    socket.on("data", (data) => {
+        const dataString = data.toString("utf-8");
+        const Id = dataString.substring(0, dataString.indexOf("-"));
+        const message = dataString.substring(dataString.indexOf("-message-") + 9);
+
+        clients.map((client) => {
+            client.socket.write(`User ${Id} : ${message}`);
+        });
+    });
+
+    // broadcasting a message when a user leaves
+    socket.on("end", (data) => {
+        clients.map((client) => {
+            client.socket.write(`User ${clientId} left!`)
+        });
+    });
+
+    clients.push({id: clientId.toString(), socket: socket});
+});
+
+server.listen(PORT, HOST, () => {
+    console.log("Opened server on ", server.address());
+});
