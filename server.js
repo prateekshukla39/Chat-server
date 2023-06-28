c830afbdecc8baccf423ee8eeb76264da44f9407
Prateek Shukla <54330564+prateekshukla39@users.noreply.github.com>
2023-06-28 17:06:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const net = require("net");
 
+const os = require('os');
+
+const interfaces = os.networkInterfaces();
+const privateIPs = [];
+
+// Iterate over each network interface
+for (const interfaceName in interfaces) {
+  const networkInterface = interfaces[interfaceName];
+  
+  // Iterate over each IP address in the network interface
+  for (const network of networkInterface) {
+    // Check for private IPv4 addresses
+    if (network.family === 'IPv4' && !network.internal) {
+      privateIPs.push(network.address);
+    }
+  }
+}
+
+console.log('Private IP addresses:');
+console.log(privateIPs);
+
 const server = net.createServer();
 
 const PORT = 4033;
-const HOST = process.env.HOST;
+const HOST = privateIPs[0];
 
 // An Array of client/socket objects 
 const clients = [];
